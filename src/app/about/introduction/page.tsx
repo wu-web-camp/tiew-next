1c11a177e25fc2e4c1d65b5a8ad02fde68ce26c0
BASIRIN <satitislam60021@gmail.com>
2025-08-03 13:20:58 +0700

    feature/introduction

---
+++ src/app/about/introduction/page.tsx
@@ -0,0 +1,10 @@
+const IntroductionPage = () => {
+  return (
+    <div>
+      <h1 className="text-2xl font-bold">Introduction</h1>
+      <p>Welcome to the introduction page!</p>
+    </div>
+  );
+};
+
+export default IntroductionPage;
